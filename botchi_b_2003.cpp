dd889e6208ed2decece75d60c789a1a4731fd569
2C2C2C <a2c2c123@126.com>
2019-01-30 16:21:15 +0800

    botchi_b_2003.cpp
    隔離された街のゲート [MISSION LEVEL: B]
    
    https://paiza.jp/botchi/challenges/botchi_b_2003

---
+++ botchi_b_2003.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // 自分の得意な言語で
+    // Let's チャレンジ！！
+
+  int mapHWN[] = { 0,0,0 };
+	int posX = 0, posY = 0, idx = 0;
+	bool correct = true;
+	std::string size_move;
+	std::string movesWa;
+	std::getline(std::cin, size_move);
+
+	// to get size and move count
+	for (int i = 0; i < 3; i++)
+	{
+		idx = size_move.find_first_of(' ');
+		mapHWN[i] = atoi(size_move.substr(0, idx).c_str());
+		size_move = size_move.substr(idx + 1, size_move.length());
+	}
+
+
+	std::string moveWa = "w";
+	for (int i = 0; i < mapHWN[2]; i++)
+	{
+		std::getline(std::cin, moveWa);
+		if (moveWa == "U")
+			posY++;
+		if (moveWa == "D")
+			posY--;
+		if (moveWa == "L")
+			posX--;
+		if (moveWa == "R")
+			posX++;
+
+		if ((posX > mapHWN[1] - 1 || posY > mapHWN[0] - 1)||(posX < 0 || posY < 0))
+			correct = false;
+	}
+	if (correct)
+		std::cout << "valid" << std::endl;
+	else
+		std::cout << "invalid" << std::endl;
+		
+    return 0;
+}
